99ee4111ee7e5424b1e7ff757edba4904886e47b
Joanna Lin <lin.joanna.y@gmail.com>
2020-02-11 04:38:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === lowercase) {
     return "I can't hear you!"
   }
-  else if (string.toUpperCase() === uppercase){
+  else if (string.toUpperCase() === uppercase) {
     return "YES INDEED!"
   }
-  else (string === "I love you, Grandma")
+  else (string === "I love you, Grandma") {
     return "I love you, too."
+  }
 }
